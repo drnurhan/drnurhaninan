aa9698417926a3207ecc76218990300e20272af8
ufukyilmazim <ufukyilmazim@gmail.com>
2026-07-25 20:36:47 +0300

    Tarayıcı diline göre otomatik yönlendirmeyi kapat
    Site artık ziyaretçinin tarayıcı/OS dili ne olursa olsun her zaman
    Türkçe (/tr) ile açılıyor; dil değişimi sadece header'daki seçiciyle.

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,9 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["tr", "en", "ar"],
   defaultLocale: "tr",
+  // Ziyaretçinin tarayıcı diline bakılmaksızın site her zaman Türkçe açılsın;
+  // dil değişimi sadece header'daki dil seçiciyle yapılır.
+  localeDetection: false,
 });
 
 export type AppLocale = (typeof routing.locales)[number];
